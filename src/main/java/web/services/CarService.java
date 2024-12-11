c77dbf9cb5e5da1001777be2bcc616af1a50841f
Mikelanjelo <m.13021999@mail.ru>
2024-12-11 20:20:29 +0300

    maybe ready

--- src/main/java/web/services/CarService.java
+++ src/main/java/web/services/CarService.java
@@ -1,33 +1,12 @@
 package web.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.controller.CarController;
-import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
-@Service
-public class CarService {
+public interface CarService {
+    List getCar(int count);
 
 
-    public List getCar(int count) {
-
-        List<Car> cars = new ArrayList<>();
-        Car car1 = new Car("BMW", 2012, 2024);
-        Car car2 = new Car("Audi", 2012, 2023);
-        Car car3 = new Car("Mercedes", 2012, 2024);
-        Car car4 = new Car("Ford", 2012, 2025);
-        Car car5 = new Car("Honda", 2012, 2026);
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-        cars.add(car4);
-        cars.add(car5);
-
-        return cars.stream().limit(count).toList();
-    }
 }
---
+++ src/main/java/web/services/CarServiceImp.java
@@ -0,0 +1,29 @@
+package web.services;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImp implements CarService {
+
+
+    public List getCar(int count) {
+
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car("BMW", 2012, 2024);
+        Car car2 = new Car("Audi", 2012, 2023);
+        Car car3 = new Car("Mercedes", 2012, 2024);
+        Car car4 = new Car("Ford", 2012, 2025);
+        Car car5 = new Car("Honda", 2012, 2026);
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+
+        return cars.stream().limit(count).toList();
+    }
+}
